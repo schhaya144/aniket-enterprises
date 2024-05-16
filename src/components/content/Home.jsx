d10b79709e0e1ecfb2b1f46111dac43a74ce1c9d
schhaya <schhaya144@gmail.com>
2024-05-16 16:39:48 +0530

    home changes

--- src/components/content/Home.jsx
+++ src/components/content/Home.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import style from "./Home.module.css";
-import heroImg from "../assets/hero.png"
+import heroImg from "../assets/hero.png";
 function Home() {
   return (
     <>
-      <div className={` ${style.containerBg}`}>
-        <div className="container pt-5">
-        <div className="row ">
-          <div className="col-md-6 d-flex justify-content-center text-align-center">
-            <h1 className="text-uppercase">We help you to grow business</h1>
-            
-          </div>
-          <div className="col-md-6 pt-5">
-            <img src={heroImg} alt="" className="w-75"/>
-          </div>
-        </div>
+      <section>
+      <div className={`mt-5 ${style.containerBg}`}>
+  <div className={`container pt- ${style.heroText}`}>
+    <div className="row pt-5 ">
+      <div className="col-12 d-flex justify-content-start">
+        <div className="text-center">
+          <h1 className="text-uppercase text-white">We will help <br />you to grow business</h1>
+          <a
+            href="#about"
+            className="btn btn-primary py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft"
+          >
+            Read More
+          </a>
         </div>
       </div>
+    </div>
+  </div>
+</div>
+
+      </section>
       <section id="about">
         <div className="container"></div>
       </section>
