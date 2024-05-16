5821a03c00037df7701c44cd675d9e6244f9a955
schhaya <schhaya144@gmail.com>
2024-05-16 13:39:25 +0530

    home changes

--- src/components/content/Home.jsx
+++ src/components/content/Home.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import style from "./Home.module.css";
+import heroImg from "../assets/hero.png"
 function Home() {
   return (
     <>
       <div className={` ${style.containerBg}`}>
         <div className="container pt-5">
         <div className="row ">
-          <div className="col-md-6 pt-5">
-            <h2>We help you to grow business</h2>
+          <div className="col-md-6 d-flex justify-content-center text-align-center">
+            <h1 className="text-uppercase">We help you to grow business</h1>
+            
           </div>
-          <div className="col-md-6"></div>
+          <div className="col-md-6 pt-5">
+            <img src={heroImg} alt="" className="w-75"/>
+          </div>
         </div>
         </div>
       </div>
