bed76d2d9db2223d60eaeca1eefdf6175c104ae5
schhaya <schhaya144@gmail.com>
2024-05-17 12:13:11 +0530

    small home chnages

--- src/components/content/Home.jsx
+++ src/components/content/Home.jsx
@@ -1,31 +1,45 @@
 import React from "react";
 import style from "./Home.module.css";
-import heroImg from "../assets/hero.png";
+// import heroImg from "../assets/hero.png";
 function Home() {
   return (
     <>
       <section>
-      <div className={`mt-5 ${style.containerBg}`}>
-  <div className={`container pt- ${style.heroText}`}>
-    <div className="row pt-5 ">
-      <div className="col-12 d-flex justify-content-start">
-        <div className="text-center">
-          <h1 className="text-uppercase text-white">We will help <br />you to grow business</h1>
-          <a
-            href="#about"
-            className="btn btn-primary py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft"
-          >
-            Read More
-          </a>
+        <div className={`mt-5 ${style.containerBg}`}>
+          <div className={`container pt- ${style.heroText}`}>
+            <div className="row pt-5 ">
+              <div className="col-12 d-flex justify-content-start">
+                <div className="text-center">
+                  <h1 className="text-uppercase text-white">
+                    We will help 
+                    you to grow business
+                  </h1>
+                  <p className="text-white fw-bold">
+                    Aniket Enterprises was set up in the year 2007as a <br />
+                    proprietorship concern. It was approved by the state
+                    government <br />of Madhya Pradesh for industrial consultancy
+                    during 2010
+                  </p>
+                  <a
+                    href="#about"
+                    className="btn btn-primary py-sm-3 px-sm-5 rounded-pill me-3 animated slideInLeft"
+                  >
+                    Read More
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-  </div>
-</div>
-
       </section>
       <section id="about">
         <div className="container"></div>
+      </section>
+      <section id="services">
+
+      </section>
+      <section className="projects">
+
       </section>
     </>
   );
--- src/components/content/Team.jsx
+++ src/components/content/Team.jsx
@@ -3,8 +3,8 @@ import "../../../src/team.css";
 
 function Team() {
   return (
-    <div className='okok'>
-     Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi qui temporibus dolorum nam nobis rerum sint. Illo velit nisi obcaecati quaerat assumenda cumque, vel a officiis nam labore accusamus eum quo quis. Tempore temporibus voluptatibus aliquam iste, magni voluptatem cum ea aperiam exercitationem. Amet, fugiat. Repellendus quaerat id possimus adipisci incidunt distinctio nesciunt atque iste.
+    <div className="container">
+      
     </div>
   )
 }
