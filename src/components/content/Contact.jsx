f4e29522c10c3eb0489b5fa4db8643a5794e1490
aish <toshiparihar121212@gmail.com>
2024-05-17 10:54:20 +0530

    some changes

---
+++ src/components/content/Contact.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Contact() {
+  return (
+    <div>
+      Service
+    </div>
+  )
+}
+
+export default Services
--- src/components/content/Team.jsx
+++ src/components/content/Team.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import "../../../src/team.css";
 
 function Team() {
   return (
-    <div>
-      Team
+    <div className='okok'>
+     Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi qui temporibus dolorum nam nobis rerum sint. Illo velit nisi obcaecati quaerat assumenda cumque, vel a officiis nam labore accusamus eum quo quis. Tempore temporibus voluptatibus aliquam iste, magni voluptatem cum ea aperiam exercitationem. Amet, fugiat. Repellendus quaerat id possimus adipisci incidunt distinctio nesciunt atque iste.
     </div>
   )
 }
