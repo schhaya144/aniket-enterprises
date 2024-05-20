25d13604c0205f2326689a4308fd9ffb9b452c6c
aish <toshiparihar121212@gmail.com>
2024-05-20 12:20:31 +0530

    some changes

--- src/components/content/About.jsx
+++ src/components/content/About.jsx
@@ -19,10 +19,10 @@ function About() {
   return (
     <div className="container about-container" id="about">
       <div className="row">
-        <div className="col-md-6">
+        <div className="col-lg-6 text-center">
           <img className="about-img img-fluid" src={whatDo} alt="" />
         </div>
-        <div className="col-md-6" id="aboutUs">
+        <div className="col-lg-6" id="aboutUs">
           <p className="about-para text-center fw-bolder">
             <h3 className="card-heading">
               <span className="about-text text-primary">
@@ -33,7 +33,7 @@ function About() {
             Our Organization i.e. Aniket Enterprises was set up in the year
             2007as a proprietorship concern. It was approved by the state
             government of Madhya Pradesh for industrial consultancy during 2010.{" "}
-            <br />
+        
             We offer multiple industrial consultancy services to our clients
             from Investment Profile, Detailed Project Report and Techno Economic
             viability reports etc <br />
@@ -46,7 +46,7 @@ function About() {
             Our wide gamut of consultancy services include preparation of
             industrial feasibility reports for banks below 1 Crore and a few
             above 1.00 Cr On an average we prepare detailed project reports for
-            more than 1000 clients every year <br />
+            more than 1000 clients every year 
             We also provide property valuation services to various banks. <br />
             We are known for our quality work, integrity and time effectiveness.{" "}
             <br />
